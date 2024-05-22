07b5132e1a4316d3e7f236154fbfa1720ecec2a0
luizpsg <luiz.saud@sga.pucminas.br>
2024-05-22 05:25:59 -0300

    chore: Refactor AopExpressions class for ordering aspects

--- src/main/java/com/luizpsg/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ src/main/java/com/luizpsg/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,13 +2,15 @@ package com.luizpsg.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
 
-  @Before("forDaoPackageNoGetterSetter()")
+  @Before("com.luizpsg.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
   public void performApiAnalytics() {
     System.out.println("\n=====>>> Performing API analytics");
   }
--- src/main/java/com/luizpsg/aopdemo/aspect/MyLoggingDemoAspect.java
+++ src/main/java/com/luizpsg/aopdemo/aspect/MyLoggingDemoAspect.java
@@ -2,11 +2,15 @@ package com.luizpsg.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
+@Order(2)
 public class MyLoggingDemoAspect {
 
-  @Before("forDaoPackageNoGetterSetter()")
+  @Before("com.luizpsg.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
   public void beforeAddAccountAdvice() {
     System.out.println("\n=====>>> Executing @Before advice on addAccount()");
   }
