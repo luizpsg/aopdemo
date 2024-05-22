fd9250acb5729c4146cc2fdad1fb9b494f743591
luizpsg <luiz.saud@sga.pucminas.br>
2024-05-22 04:48:18 -0300

    chore: Refactor AopdemoApplication to use Pointcut aspect

--- src/main/java/com/luizpsg/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luizpsg/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,72 +2,94 @@ package com.luizpsg.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+//Part 1 - AOP
+/*
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-  // this is where we add all of our related advices for logging
+  this is where we add all of our related advices for logging
 
-  // lets start with an @Before advice
+  lets start with an @Before advice
 
-  // @Before("execution(public void
-  // com.luizpsg.aopdemo.dao.AccountDAO.addAccount())")
-  // public void beforeAddAccountAdvice() {
-  // System.out.println("\n=====>>> Executing @Before advice on AccountDAO
-  // addAccount()");
-  // }
+  @Before("execution(public void
+  com.luizpsg.aopdemo.dao.AccountDAO.addAccount())")
+  public void beforeAddAccountAdvice() {
+  System.out.println("\n=====>>> Executing @Before advice on AccountDAO
+  addAccount()");
+  }
 
-  // @Before("execution(public void addAccount())")
-  // public void beforeAddAccountAdvice() {
-  // System.out.println("\n=====>>> Executing @Before advice on any void
-  // addAccount()");
-  // }
+  @Before("execution(public void addAccount())")
+  public void beforeAddAccountAdvice() {
+  System.out.println("\n=====>>> Executing @Before advice on any void
+  addAccount()");
+  }
 
-  // @Before("execution(* addAccount())")
-  // public void beforeAddAccountAdvice() {
-  // System.out.println("\n=====>>> Executing @Before advice on any
-  // addAccount()");
-  // }
+  @Before("execution(* addAccount())")
+  public void beforeAddAccountAdvice() {
+  System.out.println("\n=====>>> Executing @Before advice on any
+  addAccount()");
+  }
 
-  // @Before("execution(void add*())")
-  // public void beforeAddAccountAdvice() {
-  // System.out.println("\n=====>>> Executing @Before advice on any void
-  // add***()");
-  // }
+  @Before("execution(void add*())")
+  public void beforeAddAccountAdvice() {
+  System.out.println("\n=====>>> Executing @Before advice on any void
+  add***()");
+  }
 
-  // @Before("execution(* add*())")
-  // public void beforeAddAccountAdvice() {
-  // System.out.println("\n=====>>> Executing @Before advice on any add***()");
-  // }
+  @Before("execution(* add*())")
+  public void beforeAddAccountAdvice() {
+  System.out.println("\n=====>>> Executing @Before advice on any add***()");
+  }
 
-  // Now with params //
-  // @Before("execution(* add*(com.luizpsg.aopdemo.Account))")
-  // public void beforeAddAccountAdvice() {
-  // System.out.println("\n=====>>> Executing @Before advice on any add***(With
-  // Account param)");
-  // }
+  Now with params //
+  @Before("execution(* add*(com.luizpsg.aopdemo.Account))")
+  public void beforeAddAccountAdvice() {
+  System.out.println("\n=====>>> Executing @Before advice on any add***(With
+  Account param)");
+  }
 
-  // @Before("execution(* add*(com.luizpsg.aopdemo.Account, ..))")
-  // public void beforeAddAccountAdvice() {
-  // System.out
-  // .println("\n=====>>> Executing @Before advice on any add***(With Account
-  // param, ..*any number of params)");
-  // }
+  @Before("execution(* add*(com.luizpsg.aopdemo.Account, ..))")
+  public void beforeAddAccountAdvice() {
+  System.out
+  .println("\n=====>>> Executing @Before advice on any add***(With Account
+  param, ..*any number of params)");
+  }
 
-  // @Before("execution(* add*(..))")
-  // public void beforeAddAccountAdvice() {
-  // System.out
-  // .println("\n=====>>> Executing @Before advice on any add***(ANY number of
-  // params, and any params type)");
-  // }
+  @Before("execution(* add*(..))")
+  public void beforeAddAccountAdvice() {
+  System.out
+  .println("\n=====>>> Executing @Before advice on any add***(ANY number of
+  params, and any params type)");
+  }
 
   @Before("execution(* com.luizpsg.aopdemo.dao.*.*(..))")
   public void beforeAddAccountAdvice() {
-    System.out
-        .println(
-            "\n=====>>> Executing @Before advice on any return type of package dao on any class and any method(ANY number of params, and any params type)");
+  System.out
+  .println(
+  "\n=====>>> Executing @Before advice on any return type of package dao on any
+  class and any method(ANY number of params, and any params type)");
+  // all of this togheter will allow us to match any method in any class in the
+  // given package
+  }
+}
+*/
+
+//Part 2 - AOP with Pointcut
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+  @Pointcut("execution(* com.luizpsg.aopdemo.dao.*.*(..))")
+  private void forDaoPackage() {
+  }
+
+  @Before("forDaoPackage()")
+  public void beforeAddAccountAdviceWithPointcut() {
+    System.out.println(
+        "\n=====>>> Executing @Before advice on any return type of package dao on any class and any method(ANY number of params, and any params type) with pointcut");
     // all of this togheter will allow us to match any method in any class in the
     // given package
   }
