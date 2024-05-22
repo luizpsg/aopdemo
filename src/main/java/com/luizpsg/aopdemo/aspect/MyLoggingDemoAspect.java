f639a5be4d38bb2727d5bbdf6db64c9ace21cbb6
luizpsg <luiz.saud@sga.pucminas.br>
2024-05-22 06:40:52 -0300

    chore: Update MyLoggingDemoAspect to display method signature and arguments in beforeAddAccountAdvice

--- src/main/java/com/luizpsg/aopdemo/aspect/MyLoggingDemoAspect.java
+++ src/main/java/com/luizpsg/aopdemo/aspect/MyLoggingDemoAspect.java
@@ -1,9 +1,13 @@
 package com.luizpsg.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.luizpsg.aopdemo.Account;
 
 @Aspect
 @Component
@@ -11,8 +15,28 @@ import org.springframework.stereotype.Component;
 public class MyLoggingDemoAspect {
 
   @Before("com.luizpsg.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-  public void beforeAddAccountAdvice() {
+  public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
     System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+
+    // display the method signature
+    MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+
+    System.out.println("Method: " + methodSig);
+
+    // display the method arguments
+
+    // get args
+    Object[] args = theJoinPoint.getArgs();
+
+    // loop thru args
+    for (Object tempArg : args) {
+      System.out.println(tempArg);
+
+      if (tempArg instanceof Account theAccount) {
+        System.out.println("account name: " + theAccount.getName());
+        System.out.println("account level: " + theAccount.getLevel());
+      }
+    }
   }
 
 }
